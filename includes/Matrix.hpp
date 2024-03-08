21791c20534db5ab724c47c50cfaa66e3a78459b
Sakana <45968398+Sakana-2@users.noreply.github.com>
2024-03-08 12:01:45 +0900

    Change how to make local coordinate.

--- includes/Matrix.hpp
+++ includes/Matrix.hpp
@@ -1,6 +1,6 @@
 #pragma once
+#include <cmath>
 #include "Vec.hpp"
-
 
 class Matrix3
 {
@@ -8,14 +8,28 @@ private:
     Vec3 _a1;
     Vec3 _a2;
     Vec3 _a3;
+
 public:
-    Matrix3(const Vec3& a1,const Vec3& a2,const Vec3& a3);
-    Vec3 operator*(const Vec3& x) const;
+    Matrix3(const Vec3 &a1, const Vec3 &a2, const Vec3 &a3);
+    Vec3 operator*(const Vec3 &x) const;
 };
 
 inline Matrix3 MakeLocalCo(const Vec3 &n)
 {
-    Vec3 ex = n.cross(random_another_vec3(n)).normalize();
+
+    Vec3 x(1, 0, 0), y(0, 1, 0);
+    Vec3 another_vec;
+    float dot_x = std::fabs(x.dot(n));
+    float dot_y = std::fabs(y.dot(n));
+    if (dot_x < dot_y)
+    {
+        another_vec = x;
+    }
+    else
+    {
+        another_vec = y;
+    }
+    Vec3 ex = n.cross(another_vec).normalize();
     Vec3 ez = ex.cross(n).normalize();
-    return Matrix3(ex,n,ez);
+    return Matrix3(ex, n, ez);
 }
